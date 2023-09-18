01e0ff240328dcb2d9f4238e50bc41931dcde73d
Derek <derekdanielson84@gmail.com>
2023-09-18 09:53:27 -0600

    Added items via JS

--- app.js
+++ app.js
@@ -80,7 +80,17 @@ const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function(){
     let displayMenu = menu.map(function(item){
-        return item;
+        return `<article class="menu-item">
+        <img src=${item.img} alt=${item.title} class="photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+          </header>
+          <p class="item-text">${item.desc}</p>
+        </div>
+      </article>`;
     });
-    console.log(displayMenu);
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
 });
